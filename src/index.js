6a9b07bd5f73dba740d2588ced26a8c59bb8cc9f
Vasily Guzov <vasily.guzov@gmail.com>
2019-03-04 16:59:09 +0300

    created navigation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import * as elems from './elems';
+import navigation from './js/navigation';
 import './sass/main.scss';
-class User {
-    constructor(name){
-        this.name = name || 'some name';
-    }
 
-    getName() {
-        console.log(this.name);
-    }
-}
-
-const vaskes = new User('kinev');
-
-vaskes.getName();
+document.addEventListener('DOMContentLoaded', () => {
+    navigation();
+})
---
+++ src/js/navigation.js
@@ -0,0 +1,10 @@
+const navigation = () => {
+    const nav = document.querySelector('.navigation__btn');
+    const navMenu = document.querySelector('.navigation');
+
+    nav.addEventListener('click', () => {
+        navMenu.classList.toggle('navigation--active');
+    })
+}
+
+export default navigation;
